0a6bd90bb42dfe02f0d6c55d69b1b9c21c599041
chafiq <chafik.m149@gmail.com>
2024-10-27 09:31:38 +0100

    add environment variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplatePageComponent } from "./Layouts/template-page/template-page.component";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,13 @@ import { TemplatePageComponent } from "./Layouts/template-page/template-page.com
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+  }
   title = 'ClientApp';
 }

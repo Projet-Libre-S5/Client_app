5c794fcfbb7fd94cac6b9ebca895fe6a93a0f4ae
chafiq <chafik.m149@gmail.com>
2024-10-27 12:10:25 +0100

    Implement PrimeNg Toasts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component, isDevMode, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplatePageComponent } from "./Layouts/template-page/template-page.component";
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TemplatePageComponent],
+  imports: [RouterOutlet, TemplatePageComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),MessageService,provideAnimations()]
 };

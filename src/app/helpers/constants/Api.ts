12d101ab4c8a582d4eaf8bc7e97ff31876a2cd55
chafiq <chafik.m149@gmail.com>
2024-11-06 01:40:26 +0100

    Add adress logic implementation

---
+++ src/app/helpers/constants/Api.ts
@@ -0,0 +1,1 @@
+export const ADRESS_URL:string=`adresses`
--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  
+  private baseUrl = environment.serverUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient, @Inject(String) private url: string) { }
+
+
+
+  getById<T>(id: number | string): Observable<T> {
+    return this.http.get<T>(`${this.baseUrl}/${this.url}/${id}`);
+  }
+  
+  getAll<T>(): Observable<T> {
+    return this.http.get<T>(`${this.baseUrl}/${this.url}`);
+  }
+ 
+
+  create<T>(item: T): Observable<T> {
+    return this.http.post<T>(`${this.baseUrl}/${this.url}`, item);
+  }
+
+  update<T>(id: number | string, item: T): Observable<T> {
+    return this.http.put<T>(`${this.baseUrl}/${this.url}/${id}`, item);
+  }
+
+  delete<T>(id: number | string): Observable<T> {
+    return this.http.delete<T>(`${this.baseUrl}/${this.url}/${id}`);
+  }
 }
